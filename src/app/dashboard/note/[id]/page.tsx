def60ed3f1d34c9c633c6b5d2a756084708449fb
code043 <procoder043@gmail.com>
2024-11-01 19:34:25 -0300

    Note user page

--- src/app/dashboard/note/[id]/page.tsx
+++ src/app/dashboard/note/[id]/page.tsx
@@ -2,5 +2,16 @@ import { useParams } from "next/navigation";
 
 export default function page() {
   const { id } = useParams();
-  return <div>Note: {id}</div>;
+  return (
+    <section>
+      <div>
+        <h1>Note</h1>
+        <div>
+          <h2>Title</h2>
+          <div>Anotation</div>
+          <p>Note ID: {id}</p>
+        </div>
+      </div>
+    </section>
+  );
 }
