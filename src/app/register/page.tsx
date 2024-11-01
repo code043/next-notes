aa2a050ec48d02712efcb30b040867c4a969f025
code043 <procoder043@gmail.com>
2024-11-01 19:28:33 -0300

    Register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
-
 function page() {
-  return <div>Register</div>;
+  return (
+    <section>
+      <div>
+        <h1>Register</h1>
+        <form action="">
+          <input type="text" />
+          <input type="email" />
+
+          <input type="password" />
+          <input type="submit" value="Register" />
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default page;
