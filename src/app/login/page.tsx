0fb6f5695fa82fa5553b21c8ee1f2b7432c43874
code043 <procoder043@gmail.com>
2024-11-03 19:32:37 -0300

    Login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,22 @@
+type Note = {
+  id: string;
+  title: string;
+  content: string;
+};
+async function fetchUserNotes(): Promise<Note[]> {
+  const res = await fetch(`http://localhost:8080/api/users/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: "email@gmail.com",
+      password: "123",
+    }),
+  });
+
+  return res.json();
+}
 function page() {
   return (
     <section>
