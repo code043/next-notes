cbd64241fb221a0236d75cb34900e454af37e857
code043 <procoder043@gmail.com>
2024-11-05 16:56:58 -0300

    Create a note

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,10 +9,49 @@ async function fetchUserNotes(): Promise<Note[]> {
 
   return res.json();
 }
+
+async function createNote(): Promise<void> {
+  await fetch(`http://localhost:8080/register/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: "Nextjs",
+      content: "Learn Nextjs...",
+    }),
+  });
+}
 export default async function page() {
   const data = await fetchUserNotes();
   return (
     <section>
+      <div className="flex justify-center">
+        <form
+          action={createNote}
+          id="note-form"
+          className="flex flex-col gap-2 w-[350px] p-10 m-4 bg-blue-200 border border-radius"
+        >
+          <input
+            className="outline-none"
+            name="title"
+            type="text"
+            placeholder="Title..."
+          />
+          <textarea
+            className="h-60 outline-none resize-none"
+            placeholder="Write your note..."
+            name="content"
+            id="content"
+          ></textarea>
+          <input
+            className="bg-blue-400 cursor-pointer hover:bg-green-400 p-1"
+            id="send"
+            type="submit"
+            value="Send"
+          />
+        </form>
+      </div>
       <div>
         <ul>
           {data.map((note: Note) => {
