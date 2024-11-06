a9cb7621ba45a35bcbb8e0436b91bc4ef29b03ee
code043 <procoder043@gmail.com>
2024-11-06 19:14:30 -0300

    Styling register form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -9,15 +9,33 @@ function page() {
   }
   return (
     <section className="flex justify-center">
-      <div className="p-4 m-4">
+      <div className="bg-blue-700 p-4 m-4 rounded">
         <h1 className="text-center">Register</h1>
-        <form action={getNote} className="flex flex-col justify-center p-4">
-          <input name="name" type="text" placeholder="Name..." />
-          <input name="email" type="email" placeholder="Email..." />
-
-          <input name="password" type="password" placeholder="Password..." />
+        <form
+          action={getNote}
+          className="flex flex-col justify-center gap-2 p-4 w-[300px]"
+        >
           <input
-            className="bg-pink-500 px-4 py-1"
+            className="outline-none"
+            name="name"
+            type="text"
+            placeholder="Name..."
+          />
+          <input
+            className="outline-none"
+            name="email"
+            type="email"
+            placeholder="Email..."
+          />
+
+          <input
+            className="outline-none"
+            name="password"
+            type="password"
+            placeholder="Password..."
+          />
+          <input
+            className="text-sm font-semibold bg-pink-500 mt-2 px-4 py-1 hover:bg-green-600 cursor-pointer rounded"
             type="submit"
             value="Register"
           />
