f3da1c48dddb9adc52bfdb79f0daf9ea697b1796
code043 <procoder043@gmail.com>
2024-11-07 19:52:22 -0300

    User login with token

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,16 +1,21 @@
 import { Note } from "@/types/Note";
 
-export async function login(): Promise<Note[]> {
-  "user server";
-  const res = await fetch(`http://localhost:8080/api/users/`, {
+type Response = {
+  message: string;
+  user: { id: string; name: string };
+  token: string;
+};
+
+export async function login(user: {
+  email: string | null;
+  password: string | null;
+}): Promise<Response> {
+  const res = await fetch(`http://localhost:8080/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      email: "email@gmail.com",
-      password: "123",
-    }),
+    body: JSON.stringify(user),
   });
 
   return res.json();
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,25 @@
+"use client";
 import { login } from "@/actions/login";
 
 function page() {
-  login();
+  async function startSession(formData: FormData) {
+    const user = {
+      email: formData.get("email") as string | null,
+      password: formData.get("password") as string | null,
+    };
+    const response = await login(user);
+    console.log("resposta login ", response.token);
+    const token = response.token;
+    localStorage.setItem("user-notes", token);
+  }
+
   return (
     <section className="flex justify-center">
       <div className="p-3 m-3">
         <h1 className="text-center">Login</h1>
-        <form>
-          <input type="text" placeholder="Name..." />
-          <input type="password" placeholder="Password..." />
+        <form action={startSession}>
+          <input name="email" type="email" placeholder="Name..." />
+          <input name="password" type="password" placeholder="Password..." />
           <input
             className="bg-green-500 px-3 py-1"
             type="submit"
